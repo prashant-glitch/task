e09bf216ccf171f455fe78285cc3eaedfff74d70
prashant kumar <76524080+prashant-glitch@users.noreply.github.com>
2021-06-29 19:17:51 +0530

    Update Data.js
    Data.js has been changed

--- src/Data.js
+++ src/Data.js
@@ -1,35 +1,56 @@
-import React, { useState } from 'react';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import {useHistory} from 'react-router-dom';
-import './Data.css';
-import Axios from 'axios';
-function Data()
-{
-  const [myname,setmyname]=useState("")
-  const getmyname=()=>{
-    Axios.get("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole").then(
-      (response)=>{
-         setmyname(response.data.first + "  "+ response.data.last);
-      }
-      );
-  };
-  let history=useHistory();
-  return(
+import React, { useState, useEffect } from "react";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import { useHistory } from "react-router-dom";
+import "./Data.css";
+import axios from "axios";
+function Data() {
+  const [myname, setmyname] = useState(null);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: `https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole`,
+    })
+      .then((response) => {
+        setmyname(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(myname);
+
+  let history = useHistory();
+  return (
     <div className="main-div">
       <div className="center-div">
-         <h1>get your name <button onClick={getmyname} >my name</button></h1>
-         {myname}
-       <div className="but-div">
-          <button onClick={()=>{
-          history.push('/Time');
-          }}><NavigateBeforeIcon/></button>
-          <button onClick={()=>{
-          history.push('/Currency');
-          }}><NavigateNextIcon/></button>
+        <h1>
+          get your name <button>my name</button>
+        </h1>
+        {myname &&
+          myname.map((person) => (
+            <h1 key={person.email}>{person.first + " " + person.last}</h1>
+          ))}
+        <div className="but-div">
+          <button
+            onClick={() => {
+              history.push("/Time");
+            }}
+          >
+            <NavigateBeforeIcon />
+          </button>
+          <button
+            onClick={() => {
+              history.push("/Currency");
+            }}
+          >
+            <NavigateNextIcon />
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 export default Data;

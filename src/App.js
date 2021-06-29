e6dcc82b29b13b597f9f40d399c6e57ac22fa11f
prashant kumar <76524080+prashant-glitch@users.noreply.github.com>
2021-06-29 19:19:27 +0530

    Update App.js
    now it will open at localhost:3000

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
-import Time from './Time';
-import Data from './Data';
-import Currency from './Currency';
-import {Route, Switch} from 'react-router-dom';
-function App (){
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import Time from "./Time";
+import Data from "./Data";
+import Currency from "./Currency";
+import { Route, Switch } from "react-router-dom";
+function App() {
   return (
     <>
-  <Switch>
-    <Route exact path='/Home' component={Home} />
-    <Route path='/Time' component={Time} />
-    <Route path='/Data' component={Data}/>
-    <Route path='/Currency' component={Currency}/>
-  </Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Home" component={Home} />
+        <Route path="/Time" component={Time} />
+        <Route path="/Data" component={Data} />
+        <Route path="/Currency" component={Currency} />
+      </Switch>
     </>
   );
 }
